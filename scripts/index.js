03c230281390220e54923cb4f9dc9c4d21e2ff52
Slava Kulikov <cornelita@yandex.ru>
2025-01-26 10:30:24 +0300

    Добавлен функционал по загрузке карточек на страницу. Добавлена функция по удалению карточек со страницы

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const container = document.querySelector('.content');
+const cardsContainer = container.querySelector('.places__list');
+
+function addCards({name, link}) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = link;
+
+  deleteButton.addEventListener("click", deleteCard);
+    
+  return cardElement;
+}
+
+function deleteCard(event) {
+  const eventTarget = event.target;
+  eventTarget.parentElement.remove();
+}
+
+initialCards.map(addCards).forEach(item => cardsContainer.append(item));
+  
+
+
